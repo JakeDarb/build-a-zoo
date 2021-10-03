2cb7f49497987a2708fae9409c4681a3f663a9fc
JakeDarb <stijn.bouckaert2209@icloud.com>
2021-10-03 15:06:24 +0200

    setup[routes]

---
+++ build-a-zoo/resources/views/users/collection.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>collection</h1>
+    <p>hier komt de collection van dieren uit bijvoorbeeld de "arctic" pen</p>
+</body>
+</html>
