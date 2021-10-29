c3b377e3e2c9b046608560f4a9a224158640ac80
JurRodi <91196900+JurRodi@users.noreply.github.com>
2021-10-29 13:43:59 +0200

    Add files via upload

---
+++ resources/views/components/button.blade.php
@@ -0,0 +1,7 @@
+<div class='btn btn-{{$type ?? ''}}'>
+    {{$message ?? ''}}
+</div>
+
+<div class='btn btn-primary' href="profile/create">
+    create account(test)
+</div>
---
+++ resources/views/market/show.blade.php
@@ -0,0 +1,6 @@
+@extends('layouts/app')
+
+@section('content')
+    <h1>nft</h1>
+    <p>{{$nft}}</p>
+@endsection
